54b5ea1a2678aa068c3bc314159cb7ba2ef95ea7
Miguel Ortiz <mickyor97@gmail.com>
2025-08-26 20:17:15 -0400

    pdf generator

---
+++ forced_include_for_linter.h
@@ -0,0 +1,31 @@
+//
+// This file is not included in the notebook content
+//
+
+// Includes and defines for c++ linter
+#include <bits/stdc++.h>
+using namespace std;
+#define repx(i, a, b) for (int i = a; i < b; i++)
+#define rep(i, n) repx(i, 0, n)
+#define invrepx(i, a, b) for (int i = b - 1; i >= a; i--)
+#define invrep(i, n) invrepx(i, 0, n)
+#define eb emplace_back
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define SZ(x) ((int)x.size())
+#define pb push_back
+#define F first
+#define S second
+#define all(x) (x).begin(), (x).end()
+#define fore(i,l,r) for(int i=int(l);i<int(r);++i)
+#define SZ(x) ((int)(x).size())
+typedef long long ll;
+typedef pair<int,int> ii;
+
+
+// Segment Tree 2D
+#define MAXN 1024
+#define op(a,b) (a+b)
+#define NEUT 0
